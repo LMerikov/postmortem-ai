61cb0f937f75bd6c0abcad016f4a1737f36770ac
LMerikov <luisoleg91@gmail.com>
2026-04-06 09:02:00 -0400

    actualizar DOM

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -110,13 +110,8 @@ function triggerDownload(url, filename) {
   const link = document.createElement('a')
   link.href = url
   link.download = filename
-  link.style.display = 'none'
-  document.body.appendChild(link)
   link.click()
-  requestAnimationFrame(() => {
-    link.remove()
-    setTimeout(() => URL.revokeObjectURL(url), 100)
-  })
+  setTimeout(() => URL.revokeObjectURL(url), 100)
 }
 
 export async function exportMarkdown(postmortem) {
